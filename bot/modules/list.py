c73f8ecbab31d0e0df4e1fdff3e7567ffce6fb4c
GautamKumar <25435568+gautamajay52@users.noreply.github.com>
2020-07-22 21:45:02 +0530

    search fix for special characters

--- bot/modules/list.py
+++ bot/modules/list.py
@@ -3,10 +3,16 @@ from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
 from bot import LOGGER, dispatcher
 from bot.helper.telegram_helper.message_utils import sendMessage, editMessage
 from bot.helper.telegram_helper.filters import CustomFilters
+<<<<<<< HEAD
+=======
+import threading
+import re
+>>>>>>> e0f74e6... search fix for special characters
 from bot.helper.telegram_helper.bot_commands import BotCommands
 
 @run_async
 def list_drive(update,context):
+<<<<<<< HEAD
     try:
         search = update.message.text.split(' ',maxsplit=1)[1]
     except IndexError:
@@ -15,6 +21,11 @@ def list_drive(update,context):
         
     reply = sendMessage('Searching...', context.bot, update)
 
+=======
+    message = update.message.text
+    search = message.split(' ',maxsplit=1)[1]
+    search = re.escape(search)
+>>>>>>> e0f74e6... search fix for special characters
     LOGGER.info(f"Searching: {search}")
         
     gdrive = GoogleDriveHelper(None)
