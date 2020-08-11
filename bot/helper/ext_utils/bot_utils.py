b75d7ab0ebd1ac76ebc7971d8ab23c2d20111e8b
UsamaKarim <64292174+UsamaKarim@users.noreply.github.com>
2020-08-11 09:57:50 +0500

    Decreased spaces between Status message

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -97,13 +97,13 @@ def get_readable_message():
             if download.status() != MirrorStatus.STATUS_ARCHIVING and download.status() != MirrorStatus.STATUS_EXTRACTING:
                 msg += f"\n\n<code>{get_progress_bar_string(download)} {download.progress()}</code>" \
                        f"\n\n<b>Progress :</b> {get_readable_file_size(download.processed_bytes())}" \
-                       f"\n\n<b>Size :</b> {download.size()}" \
-                       f"\n\n<b>Speed :</b> {download.speed()} <b>| ETA :</b> {download.eta()} "
+                       f"\n<b>Size :</b> {download.size()}" \
+                       f"\n<b>Speed :</b> {download.speed()} <b>| ETA :</b> {download.eta()} "
             if download.status() == MirrorStatus.STATUS_DOWNLOADING:
                 if hasattr(download, 'is_torrent'):
-                    msg += f"\n\n<b>Peer :</b> {download.aria_download().connections} " \
+                    msg += f"\n<b>Peer :</b> {download.aria_download().connections} " \
                            f"<b>| Seed :</b> {download.aria_download().num_seeders}"
-                msg += f"\n\n<b>cancel :</b> <code>/cancel {download.gid()}</code>"
+                msg += f"\n<b>cancel :</b> <code>/cancel {download.gid()}</code>"
             msg += "\n\n"
         return msg
 
