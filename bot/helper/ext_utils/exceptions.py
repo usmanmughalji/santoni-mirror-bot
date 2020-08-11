e6b2a58fc2f19fd1f5c0b5cf7873275b23367c25
UsamaKarim <64292174+UsamaKarim@users.noreply.github.com>
2020-08-11 09:59:02 +0500

    Fixed Type

--- bot/helper/ext_utils/exceptions.py
+++ bot/helper/ext_utils/exceptions.py
@@ -1,5 +1,5 @@
 class DirectDownloadLinkException(Exception):
-    """Not method found for extracting direct download link from the http link"""
+    """No method found for extracting direct download link from the http link"""
     pass
 
 
